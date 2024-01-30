5b7ce85a28b1d19bf805ae34f13556f9f780765c
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-01-30 12:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-//complete this code
 class Animal {
-	constructor(species){
-		this.sp=species
-	}
-	get species(){
-		return this.sp;
-	}
-	 makeSound(){
-		console.log("makes sound")
-	}
+  constructor(species) {
+    this._species = species; // Use a consistent naming convention like _species
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log("makes sound");
+  }
 }
 
 class Dog extends Animal {
-	bark(){
-		console.log("woof")
-	}
+  bark() {
+    console.log("woof");
+  }
 }
 
 class Cat extends Animal {
-	purr(){
-		console.log("purr")
-	}
+  purr() {
+    console.log("purr");
+  }
 }
 
 // Do not change the code below this line
